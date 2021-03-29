e7eae371be4fe990e95850b68344793013fb2688
Francisco Farfan <francisco.farfan.lazo@gmail.com>
2021-03-28 22:34:52 -0500

    Add catching in services

--- app/services/bing_search_service.rb
+++ app/services/bing_search_service.rb
@@ -1,7 +1,11 @@
 class BingSearchService
   def self.search(query, limit = 10)
-    search_result = BingSearchApi.web(query)
-    return [] if search_result.has_key? "error"
-    return search_result[:results].slice(0, limit)
+    search_result_encoded = Rails.cache.fetch("bing-#{query}", expires_in: 1.hours) do
+      search_result = BingSearchApi.web(query)
+      return [] if search_result.has_key? "error"
+      JSON.generate(search_result)
+    end
+    Rails.cache.delete("bing-#{query}") if search_result_encoded == []
+    JSON.parse(search_result_encoded)["results"].slice(0, limit)
   end
 end
