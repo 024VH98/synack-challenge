75c8628f5593c3652802241721f34be20aee808b
Francisco Farfan <francisco.farfan.lazo@gmail.com>
2021-03-29 05:14:50 -0500

    Add Spinner

--- app/javascript/components/ResultsContainer.jsx
+++ app/javascript/components/ResultsContainer.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import RestultCard from "./ResultCard";
+import Spinner from "./Spinner";
 
 const ResultsContainer = ({ results }) => {
-  if (results === undefined) return <>Loading...</>;
+  if (results === undefined) return <Spinner />;
   if (results.length === 0) return <>No hay resultados</>;
   return (
     <>
